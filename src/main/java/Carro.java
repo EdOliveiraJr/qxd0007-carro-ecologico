2cc82186c6ba38f0b273ce27a97fe56f1b12e4a1
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2021-06-11 08:15:28 -0300

    Correção do código

--- src/main/java/Carro.java
+++ src/main/java/Carro.java
@@ -1,31 +1,120 @@
 public class Carro {
+    private int passageiros;
+    private int combustivel;
+    private int quilometragem;
+    private int maxPassageiros = 2;
+    private int maxCombustivel = 100;
+    
+    public Carro(){
+        System.out.println("O carro foi construído!!!");
+    }
 
     public int getPassageiros() {
-        return -1;
+        return passageiros;
+    }
+
+    public String mostraPassageiros(){
+        return "O carro têm" + getPassageiros() + "passageiros!";
     }
 
     public int getCombustivel() {
-        return -1;
+        return combustivel;
+    }
+
+    public String mostraCombustivel(){
+        return "O carro têm" + getCombustivel() +"litros de conbustível!";
     }
 
     public int getQuilometragem() {
-        return -1;
+        return quilometragem;
+    }
+
+    public String mostraQuilometragem(){
+        return "O carro percorreu" + getQuilometragem() +"Km no total!";
+    }
+
+    public int getMaxPassageiros(){
+        return maxPassageiros;
+    }
+
+    public int getMaxCombustivel() {
+        return maxCombustivel;         
     }
 
     public boolean embarcar() {
-        return false;
+        if(passageiros == maxPassageiros){
+            System.out.println("O carro já está lotado!");
+            return false;
+        }else{
+            passageiros++;
+            System.out.println("Passageiro entrou no carro!");
+        }
+        return true;
     }
 
     public boolean desembarcar() {
-        return false;
+        if(passageiros == 0){
+            return false;
+        }else{
+            passageiros--;
+            System.out.println("Passageiro desceu do carro!");
+        }
+        return true;
     }
 
     public boolean dirigir(int distancia) {
-        return false;
+        if(passageiros == 0 || combustivel == 0){
+            if(passageiros == 0){
+                System.out.println("Não há ninguém para dirigir!");
+            }
+            if(combustivel == 0){
+                System.out.println("Não há combutível no tanque!");
+            }
+            return false;
+        }else if(distancia == combustivel){
+            quilometragem += distancia;
+            combustivel -= distancia;
+            System.out.println("Você percorreu " + distancia + " km!!");
+            System.out.println("Você têm "+ combustivel + " litros de combustível!");
+            return true;
+        }else if(distancia >= combustivel){
+            distancia = combustivel;
+            quilometragem += distancia;
+            combustivel -= distancia;
+            System.out.println("Você percorreu " + distancia + " km!!");
+            System.out.println("Você têm "+ combustivel + " litros de combustível!");
+            System.out.println("O combustível acabou!");
+            return false;
+        }else{
+            quilometragem += distancia;
+            combustivel -= distancia;
+            System.out.println("Você percorreu " + distancia + " km!!");
+            System.out.println("Você têm "+ combustivel + " litros de combustível!");
+        }
+        return true;
     }
 
     public boolean abastecer(int quantidade) {
-        return false;
+        if(quantidade <= 0){
+            System.out.println("O quantidade de combustível tem que se maior que 0 litros!");
+            System.out.println("Você têm " + combustivel + " litros de combustivel!");
+            return false;
+        }else if(quantidade >= maxCombustivel){
+            combustivel = maxCombustivel;
+            System.out.println("O tanque foi abastecido ao máximo!");
+            System.out.println("Você têm " + combustivel + " litros de combustivel!");
+            return true;
+        }else {
+            if(combustivel + quantidade >= maxCombustivel){
+                combustivel = maxCombustivel;
+            }else{
+                combustivel += quantidade;
+            System.out.println("O tanque foi abastecido em " + quantidade + " litros!");
+            System.out.println("Você têm " + combustivel + " litros de combustivel!");
+            }
+            
+        }
+        return true;
     }
 
     @Override
